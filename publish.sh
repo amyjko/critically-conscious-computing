f1aa7850ba175f9112f8cfd3ad1ae2170456ee38
Amy J. Ko <ajko@uw.edu>
2023-02-20 14:02:07 -0800

    Migrated to Svelte-y Bookish.

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+# Exit when commands fail.
+set -e
+# Delete the reader if it's sitting around.
+if [ -d "bookish-reader" ]
+then
+    rm -rf bookish-reader
+fi
+# Clone the reader.
+git clone https://github.com/amyjko/bookish-reader
+cd bookish-reader
+# Bind the book, moving the build here.
+zsh bind.sh
+
+if [ "$1" = "preview" ]
+then
+    npx vite preview
+else
+    firebase deploy
+fi
